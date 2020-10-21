d36fc6ae36215436a45e6025927672f24f331efb
jinxiaolu <jinxiaolu16@gmail.com>
2020-10-21 21:23:11 +0800

    update

--- src/test/java/nl/martijndwars/webpush/MyTest.java
+++ src/test/java/nl/martijndwars/webpush/MyTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
 import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 
@@ -19,9 +22,8 @@ public class MyTest {
     String publicKey = "BMszYyueEqvNm8ZWFM9jjwP7lsZoOFPCSvjWrJz6mfoeI_fvcqmzRzodju-rkR7nCbwnszfkE552lQrCGrQoEJc";
     String privateKey = "LH2NrprqFIwoYRqpDcmARQ-Slj2oGz6fTqixdshUFBs";
     @Test
-    public void sendChrome() throws GeneralSecurityException, InterruptedException, JoseException, ExecutionException, IOException {
+    public void sendToChrome() throws GeneralSecurityException, InterruptedException, JoseException, ExecutionException, IOException {
         Security.addProvider(new BouncyCastleProvider());
-
         PushService pushService = new PushService()
                 .setPublicKey(publicKey)
                 .setPrivateKey(privateKey)
@@ -30,6 +32,29 @@ public class MyTest {
         String endpoint = "https://fcm.googleapis.com/fcm/send/dKgFcmb3UzQ:APA91bEXS0V8Lc8CQQ-gvie2Ji4jsBEeWjSafJ8i1clSczr7hYdLHh6dpSf3UTWjXZyK9epjOXMN3zs_ZoBjkm6EOHDPqHoGIeBP_kKcZcDf_S2I-2ZuQQlpdgQiav6a-wehErWmmlxq";
         String p256dh = "BPHniLhgMolkEDtu2j5bbPYOt9cB65vQSM2MYK0SgDHjNBvIrAOlPcj2lxKxul_lEYQFekPs-Dq8zVAe8nG2VF4";
         String auth = "BGMPkIihr-w3JniGvEokaA";
+        String payload = "{\"title\":\"title1\",\"body\":\"body1\",\"icon\":\"/html/app-manifest/logo_512.png\",\"data\":{\"url\":\"https://www.baidu.com\"}}";
+//        Notification notification = new Notification(subscription, payload);
+        Notification notification = Notification.builder()
+                .endpoint(endpoint)
+//                .topic("tp1")
+                .payload(payload)
+                .ttl(0)
+                .userPublicKey(p256dh)
+                .userAuth(auth).build();
+        HttpResponse response = pushService.send(notification);
+        System.out.println(response);
+    }
+
+    @Test
+    public void sendToFireFox() throws GeneralSecurityException, InterruptedException, JoseException, ExecutionException, IOException {
+        Security.addProvider(new BouncyCastleProvider());
+        PushService pushService = new PushService()
+                .setPublicKey(publicKey)
+                .setPrivateKey(privateKey)
+                .setSubject("mailto:admin@domain.com");
+        String endpoint = "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABfj…iLyI_ulNgwwM-4IO6RQpPES3TFCePzyP1pNj6YT_y4apwx1Vf3uQTpi5R3Sg";
+        String p256dh = "BCDjB1UjsIA2xaiwnW525x9YdWEkWtML6ajbjYveuGD78Trqfkbe4ohCoEz7tC-KqWmFsXMtiInUUsPYFAtUixE";
+        String auth = "znKI4cp4Q4QT5CXra0kXiQ";
         Subscription subscription = new Subscription(endpoint, new Subscription.Keys(p256dh, auth));
         String payload = "{\"title\":\"title1\",\"body\":\"body1\",\"icon\":\"/html/app-manifest/logo_512.png\",\"data\":{\"url\":\"https://www.baidu.com\"}}";
         Notification notification = new Notification(subscription, payload);
